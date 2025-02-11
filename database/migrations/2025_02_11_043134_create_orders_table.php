68218b1910b4d38ef7e2702dc55e11fcf320c76e
hanyseyedy <hanyseyedy@gmail.com>
2025-02-11 08:09:57 +0330

    order table migrations

--- database/migrations/2025_02_11_043134_create_orders_table.php
+++ database/migrations/2025_02_11_043134_create_orders_table.php
@@ -13,8 +13,11 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->text('product_ids'); // لیست محصولات سفارش داده شده
             $table->timestamps();
         });
+        
     }
 
     /**

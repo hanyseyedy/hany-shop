3f2722273b4bbd52a6fe40d75b2bced5a80f26bd
hanyseyedy <hanyseyedy@gmail.com>
2025-02-11 08:31:35 +0330

    create admin/users.blade content

---
+++ resources/views/admin/users.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.admin')
+
+@section('title', 'Manage Users')
+
+@section('content')
+    <h1>Manage Users</h1>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($users as $user)
+                <tr>
+                    <td>{{ $user->id }}</td>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td>
+                        <a href="{{ url('/admin/users/' . $user->id . '/edit') }}" class="btn btn-warning btn-sm">Edit</a>
+                        <form action="{{ url('/admin/users/' . $user->id) }}" method="POST" class="d-inline-block">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+@endsection
